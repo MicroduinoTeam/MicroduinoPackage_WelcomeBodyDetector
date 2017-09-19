d6bc8489a1e04ef856c3b1a227279fb202f244fe
machine1987 <maxing@microduino.cc>
2017-09-19 11:03:59 +0800

    无hub方案

--- WelcomeBodyDetector/body_welcome/userDef.h
+++ WelcomeBodyDetector/body_welcome/userDef.h
@@ -3,8 +3,8 @@
 #define Audio_SoftSerial  //Core和CoreUSB默认使用软串口
 #define MUSICVOL 30  //初始音量0~30
 
-#define PIN A0 //彩灯引脚
+#define PIN SDA //彩灯引脚
 
-#define BODY_PIN1 4  //IR引脚定义
-#define BODY_PIN2 6
+#define BODY_PIN1 A0  //IR引脚定义
+#define BODY_PIN2 A1
 
